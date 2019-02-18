f77b295ba5afef57802f7438e97b20ab7361de6d
lil-dovey98 <47420606+lil-dovey98@users.noreply.github.com>
2019-02-18 11:28:01 +0000

    Added Basic Validation
    Added a testview to transition to after successful login, login credentials are checked against a hard coded username and password (to be changed later)

--- PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/LoginActivity.java
+++ PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.MONT3.partypoolapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -172,7 +173,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             focusView = mEmailView;
             cancel = true;
         } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
+            //mEmailView.setError(getString(R.string.error_invalid_email));
+            mEmailView.setError("Username should be 6-12 characters");
+            focusView = mEmailView;
+            cancel = true;
+        }
+        else if(!email.equals("testuser") || !password.equals("password"))
+        {
+            mEmailView.setError("Incorrect Credentials");
+            mPasswordView.setError("Incorrect Credentials");
             focusView = mEmailView;
             cancel = true;
         }
@@ -192,7 +201,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        //return email.contains("@");
+
+        return (email.length() >= 6 && email.length() <= 12);
     }
 
     private boolean isPasswordValid(String password) {
@@ -333,7 +344,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent myIntent = new Intent(LoginActivity.this, TestActivity.class);
+                LoginActivity.this.startActivity(myIntent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
