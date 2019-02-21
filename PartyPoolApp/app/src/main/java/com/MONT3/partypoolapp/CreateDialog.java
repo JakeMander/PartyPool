8e75bf8fd577dbd596cfd5ed0c916d145cbf4513
lil-dovey98 <47420606+lil-dovey98@users.noreply.github.com>
2019-02-21 15:39:23 +0000

    Added Dialog Box
    On button press, creates a dialog box for creating a party that shows the option of 2 radio buttons, functionality to be added at a later date

---
+++ PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/CreateDialog.java
@@ -0,0 +1,35 @@
+package com.MONT3.partypoolapp;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class CreateDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog,null);
+
+        builder.setView(view)
+                .setTitle("Choose Voting Mode")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
--- PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/TestActivity.java
+++ PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/TestActivity.java
@@ -2,10 +2,13 @@ package com.MONT3.partypoolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
-
+private Button buttonJoin;
+private Button buttonCreate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +16,19 @@ public class TestActivity extends AppCompatActivity {
 
         TextView tv = (TextView) findViewById(R.id.textView2);
         tv.setText("Welcome, " + getIntent().getStringExtra("PASSDATA"));
+        buttonJoin = (Button) findViewById(R.id.button2);
+        buttonCreate = (Button) findViewById(R.id.button3);
+
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog()
+    {
+        CreateDialog dialog1 = new CreateDialog();
+        dialog1.show(getSupportFragmentManager(),"create dialog");
     }
 }
