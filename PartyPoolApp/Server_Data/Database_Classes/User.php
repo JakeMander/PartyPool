8b30e374b99caa12c4e891112abd5c095fd08b17
Jake Mander <32242126+JakeMander@users.noreply.github.com>
2019-03-03 21:58:22 +0000

    Added PartyPool Web Service API + Restructured Folders
    Added PHP API File To Project. This Will Override The Basic Functionallity of Wayne's RestService.php Class And Allow Us To Add Our Own Functionallity For HTTP Functions Such As POST, GET And Others If Required.

--- PartyPoolApp/Server_Data/Database_Classes/User.php
+++ PartyPoolApp/Server_Data/Database_Classes/User.php
@@ -45,3 +45,5 @@ class User
         return $this->created;
     }
 }
+
+?>
---
+++ PartyPoolApp/Server_Data/PartyPool_API.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jake Mander
+ * Date: 03/03/2019
+ * Time: 19:42
+ */
+
+/*
+ *  All Requests Made To The Server Will Go Through The PartyPool API. Each Requests Invokes The Base Class Method
+ *  handleRawRequest Which Strips The Components Embedded In The URL And Passes The Parameters On To The handleRequest
+ *  method. This Then Determines The Type Of The HTTP Request And Then
+ *
+ *
+ *
+ */
+?>
