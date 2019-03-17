5bf7e02c56500f5bc2876a2956ec47c0c58db501
Jake Mander <32242126+JakeMander@users.noreply.github.com>
2019-03-17 20:56:34 +0000

    Removed "Loader" Methods And Interface Implementation
    -Removed "Implements LoaderCallbacks<cursor>. This interface was being used to pull in contact details from the users device (which is unnecessary).
    - Removed all loader methods. These were being used to pull email addresses from the users device. This is unnecessary boilerplate code.

--- PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/LoginActivity.java
+++ PartyPoolApp/app/src/main/java/com/MONT3/partypoolapp/LoginActivity.java
@@ -38,7 +38,7 @@ import java.util.List;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -113,6 +113,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
 
     private void attemptLogin() {
+
+        //  If No Asynchronous Task Is Set To Be Called Upon Credentials Being Successfully
+        // Validated On The Client Side, Exit The Method.
         if (mAuthTask != null) {
             return;
         }
@@ -143,7 +146,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             cancel = true;
         }
 
-        // Checks To See If Username Is Empty.
+        // Checks To See If The String Retrieved From The Username TextView Is Empty.
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
@@ -227,61 +230,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
+    
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
